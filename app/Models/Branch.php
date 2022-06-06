4900db42eb15f5d1313a74aad9acd5d10c5f443b
mayorgeek <isaacadepoju2004@gmail.com>
2022-06-06 15:23:25 +0100

    Fixed branch has announcements

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Event;
 use App\Models\Tithe;
 use App\Models\Offering;
+use App\Models\Announcement;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -25,6 +26,10 @@ class Branch extends Model
 
     public function events(): HasMany {
         return $this->hasMany(Event::class);
+    }
+
+    public function announcement(): HasMany {
+        return $this->hasMany(Announcement::class);
     }
 
 }
