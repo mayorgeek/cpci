dd38f3b2b2dd301416904578a1b7b056e946a4d4
mayorgeek <isaacadepoju2004@gmail.com>
2022-06-18 23:06:56 +0100

    feature: changed order of activitylogs and userlogs table to latest

--- app/Filament/Resources/ActivityLogsResource/Pages/ListActivityLogs.php
+++ app/Filament/Resources/ActivityLogsResource/Pages/ListActivityLogs.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\ActivityLogsResource\Pages;
 
 use App\Filament\Resources\ActivityLogsResource;
+use App\Models\ActivityLog;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListActivityLogs extends ListRecords
 {
     protected static string $resource = ActivityLogsResource::class;
+
+    protected function getTableQuery(): Builder
+    {
+        return ActivityLog::query()->orderBy('created_at', 'desc');
+    }
+
 }
--- app/Filament/Resources/UserLogsResource/Pages/ListUserLogs.php
+++ app/Filament/Resources/UserLogsResource/Pages/ListUserLogs.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\UserLogsResource\Pages;
 
 use App\Filament\Resources\UserLogsResource;
+use App\Models\UserLog;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListUserLogs extends ListRecords
 {
     protected static string $resource = UserLogsResource::class;
+
+    protected function getTableQuery(): Builder
+    {
+        return UserLog::query()->orderBy('created_at', 'desc');
+    }
+
 }
